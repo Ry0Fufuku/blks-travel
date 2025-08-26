61d77f6f8d19a799ab5d3140050a1738d548fbea
RYO FUKUDA <lothwxr@gmail.com>
2025-08-26 14:12:36 +0900

    確認お願いします

---
+++ blkstravel/src/main/java/com/example/blkstravel/BlkstravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.blkstravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlkstravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlkstravelApplication.class, args);
+	}
+
+}
---
+++ blkstravel/src/test/java/com/example/blkstravel/BlkstravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blkstravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlkstravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
